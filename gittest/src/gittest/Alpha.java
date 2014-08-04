2eb7526b68bfc1c03dd6f35b48356b0fd7905c9e
minthaka <mohareve@minthaka>
2014-08-04 18:56:05 +0200

    Added getter and setter for name field.

--- gittest/src/gittest/Alpha.java
+++ gittest/src/gittest/Alpha.java
@@ -2,4 +2,12 @@ package gittest;
 
 public class Alpha {
 private String name;
+
+public synchronized String getName() {
+	return name;
+}
+
+public synchronized void setName(String name) {
+	this.name = name;
+}
 }
